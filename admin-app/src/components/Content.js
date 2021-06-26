e4b8155416c07434fc72f8a2e87f3437e357c1a6
Ayo <bimboladxtreme@gmail.com>
2021-06-26 14:55:00 +0100

    Some styling on header section

--- admin-app/src/components/Content.js
+++ admin-app/src/components/Content.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Container, Row } from "react-bootstrap";
+import {
+	Container,
+	Row,
+	Dropdown,
+	DropdownButton,
+	Card,
+} from "react-bootstrap";
+
+import { FiFilter, FiRefreshCw } from "react-icons/fi";
 import styled from "styled-components";
 import AccordionTest from "../AccordionTest";
 import { overviewInfo } from "../appData";
@@ -20,13 +28,38 @@ function Content() {
 										Welcome back, Ayo!
 									</h4>
 								</div>
-								<div className="section__header-right">Today</div>
+								<div className="section__header-right flexed">
+									<DropdownButton
+										id="dropdown-basic-button"
+										title="Today"
+										className="dropdown__header">
+										<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+										<Dropdown.Item href="#/action-2">
+											Another action
+										</Dropdown.Item>
+										<Dropdown.Item href="#/action-3">
+											Something else
+										</Dropdown.Item>
+										<Dropdown.Divider />
+										<Dropdown.Item href="#/action-3">
+											Seperated Link
+										</Dropdown.Item>
+									</DropdownButton>
+									<div className="flexed">
+										<span className="header__right-icon p-2 mx-1 ">
+											<FiFilter />
+										</span>
+										<span className="header__right-icon p-2 mx ">
+											<FiRefreshCw />
+										</span>
+									</div>
+								</div>
 							</div>
 							<div className="overview__wrap my-2 flexed flex-wrap mx-auto">
 								{overviewInfo.map((info) => {
 									const { icon, iconColor, figure, text } = info;
 									return (
-										<div className="overview__container bg-success mx-2 py-2 my-2 col-sm-10 col-md-5 col-lg-3 flexed">
+										<div className="overview__container bg-success mx-2 py-2 my-2 col-sm-10 col-md-5 col-lg flexed">
 											<span className={`${iconColor} overview__icon`}>
 												{" "}
 												{icon}{" "}
